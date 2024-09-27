f2c4c74588d0310104e4e492660b4b3c004c39f3
Neil Stevenson <neil.github@secureness.co.uk>
2024-09-27 15:21:24 +0100

    Update to motor control for latest ESP32 code base

--- dragster/MMDragsterGame/motors.cpp
+++ dragster/MMDragsterGame/motors.cpp
@@ -20,6 +20,11 @@ void Motors::initHardware()
 #endif //MOTORCTRL_TB6612
 
 #ifdef MOTORCTRL_DRV8833
+  ledcAttach(gpioMotorA1, pwmFreq, pwmResolution);
+  ledcAttach(gpioMotorA2, pwmFreq, pwmResolution);
+  ledcAttach(gpioMotorB1, pwmFreq, pwmResolution);
+  ledcAttach(gpioMotorB2, pwmFreq, pwmResolution);
+  /* For 2.x ESP32
   ledcSetup(motorA1PwmChannel, pwmFreq, pwmResolution);
   ledcAttachPin(gpioMotorA1, motorA1PwmChannel);
   ledcSetup(motorA2PwmChannel, pwmFreq, pwmResolution);
@@ -29,7 +34,8 @@ void Motors::initHardware()
   ledcAttachPin(gpioMotorB1, motorB1PwmChannel);
   ledcSetup(motorB2PwmChannel, pwmFreq, pwmResolution);
   ledcAttachPin(gpioMotorB2, motorB2PwmChannel);
-  
+  */
+
 #endif //MOTORCTRL_DRV8833
 }
 
@@ -46,10 +52,16 @@ void Motors::stopAll()
     ledcWrite(motorLPwmChannel, 0);
 #endif //MOTORCTRL_TB6612
 #ifdef MOTORCTRL_DRV8833
+    ledcWrite(gpioMotorA1, 0);
+    ledcWrite(gpioMotorA2, 0);
+    ledcWrite(gpioMotorB1, 0);
+    ledcWrite(gpioMotorB2, 0);
+  /* For 2.x ESP32
     ledcWrite(motorA1PwmChannel, 0);
     ledcWrite(motorA2PwmChannel, 0);
     ledcWrite(motorB1PwmChannel, 0);
     ledcWrite(motorB2PwmChannel, 0);
+  */
 #endif //MOTORCTRL_DRV8833
     digitalWrite(gpioIlluminationLED, LOW);
 }
@@ -74,15 +86,27 @@ void Motors::setSpeed(int forwardSpeed)
 #endif //MOTORCTRL_TB6612
 #ifdef MOTORCTRL_DRV8833
 #ifdef MOTORCTRL_WITHBREAK
+      ledcWrite(gpioMotorA1, 256);
+      ledcWrite(gpioMotorA2, 255-dutyCycle);
+      ledcWrite(gpioMotorB1, 256);
+      ledcWrite(gpioMotorB2, 255-dutyCycle);
+/* For 2.x ESP32
       ledcWrite(motorA1PwmChannel, 256);
       ledcWrite(motorA2PwmChannel, 255-dutyCycle);
       ledcWrite(motorB1PwmChannel, 256);
       ledcWrite(motorB2PwmChannel, 255-dutyCycle);
+*/
 #else //MOTORCTRL_WITHBREAK
+      ledcWrite(gpioMotorA1, dutyCycle);
+      ledcWrite(gpioMotorA2, 0);
+      ledcWrite(gpioMotorB1, dutyCycle);
+      ledcWrite(gpioMotorB2, 0);
+/* For 2.x ESP32
       ledcWrite(motorA1PwmChannel, dutyCycle);
       ledcWrite(motorA2PwmChannel, 0);
       ledcWrite(motorB1PwmChannel, dutyCycle);
       ledcWrite(motorB2PwmChannel, 0);
+*/
 #endif //MOTORCTRL_WITHBREAK
 #endif //MOTORCTRL_DRV8833
     }
@@ -100,15 +124,27 @@ void Motors::setSpeed(int forwardSpeed)
 #endif //MOTORCTRL_TB6612
 #ifdef MOTORCTRL_DRV8833
 #ifdef MOTORCTRL_WITHBREAK
+      ledcWrite(gpioMotorA1, 255-dutyCycle);
+      ledcWrite(gpioMotorA2, 256);
+      ledcWrite(gpioMotorB1, 255-dutyCycle);
+      ledcWrite(gpioMotorB2, 256);
+/* For 2.x ESP32
       ledcWrite(motorA1PwmChannel, 255-dutyCycle);
       ledcWrite(motorA2PwmChannel, 256);
       ledcWrite(motorB1PwmChannel, 255-dutyCycle);
       ledcWrite(motorB2PwmChannel, 256);
+*/      
 #else //MOTORCTRL_WITHBREAK
+      ledcWrite(gpioMotorA1, 0);
+      ledcWrite(gpioMotorA2, dutyCycle);
+      ledcWrite(gpioMotorB1, 0);
+      ledcWrite(gpioMotorB2, dutyCycle);
+/* For 2.x ESP32
       ledcWrite(motorA1PwmChannel, 0);
       ledcWrite(motorA2PwmChannel, dutyCycle);
       ledcWrite(motorB1PwmChannel, 0);
       ledcWrite(motorB2PwmChannel, dutyCycle);
+*/      
 #endif //MOTORCTRL_WITHBREAK
 #endif //MOTORCTRL_DRV8833
     }
@@ -127,9 +163,15 @@ void Motors::breakStop()
     ledcWrite(motorLPwmChannel, 0);
 #endif //MOTORCTRL_TB6612
 #ifdef MOTORCTRL_DRV8833
+    ledcWrite(gpioMotorA1, 256);
+    ledcWrite(gpioMotorA2, 256);
+    ledcWrite(gpioMotorB1, 256);
+    ledcWrite(gpioMotorB2, 256);
+/* For 2.x ESP32
     ledcWrite(motorA1PwmChannel, 256);
     ledcWrite(motorA2PwmChannel, 256);
     ledcWrite(motorB1PwmChannel, 256);
     ledcWrite(motorB2PwmChannel, 256);
+*/
 #endif //MOTORCTRL_DRV8833
 }
